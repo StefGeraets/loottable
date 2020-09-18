c76dd54ec3ef5059f60e6100cebe5763d050e84c
Stef Geraets <stef@getnoticed.nl>
2020-09-18 11:14:04 +0200

    First deploy

---
+++ precache-manifest.7c948e6f0d508367ff002ce6228129a5.js
@@ -0,0 +1,39 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3eb9410a7b8b4ccfc8f",
+    "url": "bundle.css"
+  },
+  {
+    "revision": "e3eb9410a7b8b4ccfc8f",
+    "url": "bundle.js"
+  },
+  {
+    "revision": "420f6398cf0cee5cbda8db217e58cdf5",
+    "url": "favicon.png"
+  },
+  {
+    "url": "icon_128x128.962085a805f94fa1ec5eb08d5396d827.png"
+  },
+  {
+    "url": "icon_192x192.7ffdef9aa4e01db8dc58005aa47c5096.png"
+  },
+  {
+    "url": "icon_256x256.81e7c9ee64b186be6120e9c74d3c9fa8.png"
+  },
+  {
+    "url": "icon_384x384.7c875f808e381dd861c2063b520271e6.png"
+  },
+  {
+    "url": "icon_512x512.c6f067da61dfd585a057bbff86609beb.png"
+  },
+  {
+    "url": "icon_96x96.9b004573aa0135623e921edec7c8d134.png"
+  },
+  {
+    "revision": "8c1518ebc84d26727a705bd1ebaad4c2",
+    "url": "index.html"
+  },
+  {
+    "url": "manifest.8d7e92231649ddae18726bf58a5a2cba.json"
+  }
+]);

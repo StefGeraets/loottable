0d2234d168d6eb36e8373e807b865100c1118c88
Stef Geraets <stef@getnoticed.nl>
2020-09-18 20:50:41 +0200

    Add readme and tables

---
+++ README.md
@@ -0,0 +1,56 @@
+# LootTable Generator
+
+Inspired by the revamped [Treasure/Loot table](https://www.youtube.com/watch?v=U1gQYS8GyaE) from [Jacob Budz](https://twitter.com/XPtoLevel3 "Jacob's Twitter") I build this simple generator.
+
+It can be used on [github pages](https://stefgeraets.github.io/loottable/)
+
+It is used during or before a game of Dungeons and Dragons 5e to generate individual monster loot or hoard treasure based on [this table](https://github.com/StefGeraets/loottable/blob/master/files/).
+
+## Install
+
+**To install the app:**
+
+```
+npm install
+```
+or
+```
+yarn install
+```
+
+**Run the app locally:**
+```
+npm run dev
+```
+or
+```
+yarn dev
+```
+
+**Build the app:**
+```
+npm run build
+```
+or
+```
+yarn build
+```
+
+## Known Issues
+
+- [ ] Form Error handling
+- [ ] Bad code. Needs refactoring
+
+## Future features
+
+- [ ] Roll random Gem colours from DMG
+- [ ] Re-roll random Gem colour
+- [ ] Roll random art objects from DMG
+- [ ] Re-roll individual art objects
+- [ ] Roll random magic items from [provided table](https://github.com/StefGeraets/loottable/blob/master/files/)
+- [ ] Re-roll individual art objects
+- [ ] User customized magic item table
+
+
+## Find any bugs?
+Please let them know here on [Github](https://github.com/StefGeraets/loottable) or via [Twitter](https://twitter.com/StefGeraets)
--- src/data/art.js
+++ src/data/art.js
@@ -1,6 +1,75 @@
 export const artTable = [
   {
     worth: "25GP",
-    
+    list: [
+      "Silver Ewer",
+      "Carved bone statuette",
+      "Small gold bracelet",
+      "Cloth-of-gold vestments",
+      "Black velvet mask stitched with silver thread",
+      "Copper chalice with silver filigree",
+      "Pair of engraved bone dice",
+      "Small mirror set in a painted wooden frame",
+      "Embroidered silk handkerchief",
+      "Gold locket with a painted portrait inside"
+    ]
+  },
+  {
+    worth: "250GP",
+    list: [
+      "Gold ring set with bloodstones",
+      "Carved ivory statuette",
+      "Large gold bracelet",
+      "Silver necklace with a gemstone pendant",
+      "Bronze crown",
+      "Silk robe with gold embroidery",
+      "Large well-made tapestry",
+      "Brass mug with jade inlay",
+      "Box of turquoise animal figurines",
+      "Gold bird cage with electrum filigree"
+    ]
+  },
+  {
+    worth: "750GP",
+    list: [
+      "Silver chalice set with moonstones",
+      "Silver-plated steellongsword with jet set in hilt",
+      "Carved harp of exotic wood with ivory inlay and zircon gems",
+      "small gold idol",
+      "Gold dragon comb set with red garnets as eyes",
+      "Bottle stopper cork embossed with gold lead and set with amethysts",
+      "Ceremonial electrum dagger with a black pearl in the pommel",
+      "Silver and gold brooch",
+      "Obsidian statuette with gold fittings and inlay",
+      "Painted gold war mask"
+    ]
+  },
+  {
+    worth: "2500GP",
+    list: [
+      "Fine gold chain set with a fire opal",
+      "Old masterpiece painting",
+      "Embroidered silk an velvet mantle set with numerous moonstones",
+      "Platinum bracelet set with a sapphire",
+      "Embroidered glove set with jewel chips",
+      "Jeweled anklet",
+      "Gold music box",
+      "Gold criclet set with four aquamarines",
+      "Eye path with a mock eye set in blue sapphire and moonstones",
+      "A necklace string of small pink pearls"
+    ]
+  },
+  {
+    worth: "7500GP",
+    list: [
+      "Jeweled gold crown",
+      "Jeweled platinum ring",
+      "Small gold statuette set with rubies",
+      "Gold cup set with emeralds",
+      "Gold jewelry box with platinum filigree",
+      "Painted gold child's sarcophagus",
+      "Jade game board with solid gold playing pieces",
+      "Bejeweled ivory drinking horn with gold filigree"
+    ]
   }
 ]
